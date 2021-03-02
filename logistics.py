a16e78075b0e9646cb3da1d16aa4f72836f5fd47
bogdanmagometa <bogdan.magometa@gmail.com>
2021-03-03 00:25:29 +0200

    Implemented logistics.py and order.py

--- logistics.py
+++ logistics.py
@@ -1,17 +1,90 @@
+"""
+logistics.py
+
+A module for a single class LogisticSystem.
+Refer to LogisticSystem documentation for more information.
+"""
+
 import copy
 from typing import List
 
 from order import Vehicle
 from order import Order
+from order import Item
 
 
 class LogisticSystem:
+    """
+    A class used to represent logistics system.
+
+    Attributes
+    ----------
+    orders : list of orders
+        all orders known to logistics system
+    vehicles : list of vehicles
+        all vehicles known to logistics system
+
+    Methods
+    -------
+    placeOrder(order)
+        assign an order a vehicle>>> from order import *
+    trackOrder(order_id)
+        print information about an order with passed order id
+    """
     def __init__(self, vehicles: List[Vehicle]) -> None:
+        """Initialize logistics system with vehicles andd orders (empty list).
+        
+        >>> vehicles = [Vehicle(1), Vehicle(10)]
+        >>> logistics_sys = LogisticSystem(vehicles)
+        >>> logistics_sys.placeOrder(Order("Bohdan", "Lviv", 2, [Item("Arduino", 200)]))
+        Your order number is 0.
+        >>> logistics_sys.trackOrder(0)
+        Your order #0 is sent to Lviv. Total price: 200 UAH.
+        """
         self.orders = []
         self.vehicles = copy.copy(vehicles)
 
-    def place_order(self, order: Order) -> None:
-        pass
+    def placeOrder(self, order: Order) -> None:
+        """Assign a vehicle to an order and add an order to orders of logistics system.
 
-    def track_order(self, order_id: int) -> str:
-        pass
+        >>> vehicles = [Vehicle(1)]
+        >>> logistics_sys = LogisticSystem(vehicles)
+        >>> logistics_sys.placeOrder(Order("Jack", "London", 4, [Item("Arduino mega", 200)]))
+        Your order number is 1.
+        >>> logistics_sys.placeOrder(Order("Tom", "Kyiv", 31, [Item("Cap", 120)]))
+        Your order number is 2.
+        There is no available vehicle to deliver an order.
+        >>> logistics_sys.orders #doctest: +ELLIPSIS
+        [<order.Order object at 0x...>]
+        """
+        try:
+            vehicle = next(vehicle for vehicle in self.vehicles if vehicle.is_available)
+            self.orders.append(order)
+            vehicle.is_available = False
+        except StopIteration:
+            print("There is no available vehicle to deliver an order.")
+
+    def trackOrder(self, order_id: int) -> str:
+        """Print information (order id, city of delivery, price in UAH) about the order with passed
+        in order id.
+
+        >>> vehicles = [Vehicle(1)]
+        >>> logistics_sys = LogisticSystem(vehicles)
+        >>> logistics_sys.placeOrder(Order("Jack", "London", 4, [Item("Arduino mega", 200)]))
+        Your order number is 3.
+        >>> logistics_sys.trackOrder(3)
+        Your order #3 is sent to London. Total price: 200 UAH.
+        """
+        try:
+            order = next(order for order in self.orders if order.order_id == order_id)
+            order_id = order.order_id
+            city = order.location.city
+            amount = order.calculate_amount()
+            print(f"Your order #{order_id} is sent to {city}. Total price: {amount} UAH.")
+        except StopIteration:
+            print("No such order.")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- order.py
+++ order.py
@@ -1,45 +1,178 @@
+"""
+order.py
+
+This module contains classes needed for emulating logistics system. In particular, the following
+classes are here:
+Item
+Vehicle
+Order
+Location
+"""
+
 import copy
 from typing import List
 
 
 class Item:
+    """A class used to represent an item for logistics system.
+    
+    Attributes
+    ----------
+    name : str
+        a name of the item, e.g. book, letter, TV, cookie
+    price : float
+        the price of an item in UAH
+    """
     def __init__(self, name: str, price: float) -> None:
+        """Initialize Item with name and price (in UAH).
+
+        >>> item = Item("phone", 5123.4567)
+        >>> item.name
+        'phone'
+        >>> item.price
+        5123.4567
+        """
         self.name = name
         self.price = price
 
     def __str__(self) -> str:
-        print(f"The item with name {self.name} and price {self.price}")
+        """Return human-readable representation of the order.
 
-
-class Order:
-    num_orders_created = 0
-
-    def __init__(self, user_name: str, city: str, post_office: int, items: List[Item]) -> None:
-        self.order_id = num_orders_created
-        self.user_name = user_name
-        self.location = Location(city, post_office)
-        self.items = copy.copy(items)
-        self.vehicle = None
-        num_orders_created += 1
-
-    def __str__(self) -> str:
-        return f"The order #{self.order_id} by {self.user_name} to city {self.location.city}, post\
-office {self.location.post_office}. The items are {','.join(self.items)}."
-
-    def calculate_amount(self) -> float:
-        return sum(item.price for item in self.items)
-
-    def assign_vehicle(self, vehicle: Vehicle) -> None:
-        self.vehicle = vehicle
+        >>> item = Item("shoes", 240)
+        >>> print(item)
+        The item with name shoes and price 240
+        """
+        return f"The item with name {self.name} and price {self.price}"
 
 
 class Vehicle:
-    def __init__(self, vehicle_no: int, is_available: bool) -> None:
+    """A class user to represent Vehicles for logistics system.
+
+    Attributes
+    ----------
+    vehicle_no : int
+        number of vehicle
+    is_available : bool
+        tells if a vehicle is available for delivering
+    """
+    def __init__(self, vehicle_no: int) -> None:
+        """Initialize Vehicle with vehicle number."""
         self.vehicle_no = vehicle_no
-        self.is_available = is_available
+        self.is_available = True
+
+
+class Order:
+    """A class used to represent an order in logistics system.
+
+    Attributes
+    ----------
+    user_name : str
+        the name of the user who created the order
+    city : str
+        the city of destination
+    postoffice : int
+        the postoffice number of Ukrposhta in the city
+    items : list of items
+        items listed in the order
+    location : Location
+        location of destination point
+    vehicle : Vehicle
+        vehicle for delivery of the item
+    """
+    num_orders_created = 0
+
+    def __init__(self, user_name: str, city: str, postoffice: int, items: List[Item]) -> None:
+        """Initialize order with name of user, delivery city, postoffice, and items to deliver.
+
+        >>> order = Order("Bohdan", "Stryi", 2,
+        ... [Item('Arduino',120), Item("ESP32-CAM",200), Item("Raspberri Pi Zero",1100)])
+        Your order number is 0.
+        >>> isinstance(order.location, Location)
+        True
+        >>> order.vehicle
+        >>> order.user_name
+        'Bohdan'
+        >>> order.location.city
+        'Stryi'
+        >>> order.location.postoffice
+        2
+        >>> all(map(lambda x: isinstance(x, Item), order.items))
+        True
+        """
+        self.order_id = Order.num_orders_created
+        self.user_name = user_name
+        self.location = Location(city, postoffice)
+        self.items = copy.copy(items)
+        self.vehicle = None
+        Order.num_orders_created += 1
+        print(f"Your order number is {self.order_id}.")
+
+    def __str__(self) -> str:
+        """Return human-readable represenation of an order.
+        
+        >>> order = Order("Ivan", "Kyiv", "42", ['computer'])
+        Your order number is 1.
+        >>> print(order)
+        The order #1 by Ivan to city Kyiv, postoffice 42. The item is computer.
+        """
+        text = f"The order #{self.order_id} by {self.user_name} to city {self.location.city}, post\
+office {self.location.postoffice}."
+
+        if self.items:
+            text += " The item"
+            if len(self.items) == 1:
+                return text + f" is {self.items[0]}."
+            return text + f"s are {', '.join(self.items)}."
+        return text
+
+    def calculate_amount(self) -> float:
+        """Return total cost of each Item (in UAH).
+
+        >>> order = Order("Bohdan", "Stryi", "2",
+        ... [Item('Arduino',120), Item("ESP32-CAM",200),
+        ... Item("Raspberri Pi Zero",1100)]) #doctest: +ELLIPSIS
+        Your order number is ....
+        >>> order.calculate_amount()
+        1420
+        """
+        return sum(item.price for item in self.items)
+
+    def assign_vehicle(self, vehicle: Vehicle) -> None:
+        """Assign a vehicle to an order.
+
+        >>> order = Order("Oksana", "Zhytomyr", 5, [Item("cap", 100)]) #doctest: +ELLIPSIS
+        Your order number is ....
+        >>> vehicle = Vehicle(213)
+        >>> order.assign_vehicle(vehicle)
+        >>> order.vehicle.vehicle_no
+        213
+        """
+        self.vehicle = vehicle
 
 
 class Location:
-    def __init__(self, city: str, post_office: int) -> None:
+    """A class used to represent a location in logistics system.
+
+    Attributes
+    ----------
+    city : str
+        city of the location
+    postoffice : int
+        number of postoffice of Ukrposhta in city
+    """
+    def __init__(self, city: str, postoffice: int) -> None:
+        """Initialize location with delivery city and postoffice.
+
+        >>> location = Location("Nezhukhiv", 1)
+        >>> location.city
+        'Nezhukhiv'
+        >>> location.postoffice
+        1
+        """
         self.city = city
-        self.post_office = post_office
+        self.postoffice = postoffice
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
